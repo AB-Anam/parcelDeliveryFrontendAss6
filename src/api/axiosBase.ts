42376c4f90b84a7a1e75546ae0b4ca873aa133e3
abanam <abanam275@gmail.com>
2025-10-06 16:15:03 +0600

    axiosBase.ts is done

--- src/api/axiosBase.ts
+++ src/api/axiosBase.ts
@@ -1,14 +1,16 @@
+// axios instance
 import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000",
-  withCredentials: true,
+  withCredentials: true // if you rely on cookies from backend
 });
 
+// attach client-managed token (if you store token in cookie non-httpOnly)
 axiosInstance.interceptors.request.use((config) => {
   const token = Cookies.get("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 

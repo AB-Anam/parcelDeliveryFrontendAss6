6fe08642deb311290c8943e8b43beade70940c1c
abanam <abanam275@gmail.com>
2025-10-06 16:16:58 +0600

    config files updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path' // <-- Add this import
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // <-- Add this resolve block
-    },
-  },
-})
+});
